7b250d6977739f861d92d55f3bf6c1ec9cb82937
Vinson Martin <vm84039@yahoo.com>
2023-11-08 12:02:50 -0700

    update

--- src/main/java/com/cognixia/jump/Scheduler.java
+++ src/main/java/com/cognixia/jump/Scheduler.java
@@ -1,6 +1,5 @@
 package com.cognixia.jump;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -8,26 +7,25 @@ import org.springframework.stereotype.Component;
 import com.cognixia.jump.model.News.Category;
 import com.cognixia.jump.service.NewsApiService;
 
-
-
 @Component
 public class Scheduler {
-	
-	@Autowired
-	NewsApiService newsService;
-	
-	@Scheduled(fixedRate = 3600000) // 1 hour in milliseconds
+
+    @Autowired
+    NewsApiService newsService;
+
+    @Scheduled(initialDelay = 0, fixedRate = 5400000) // 1 hour in milliseconds
     public void runScheduledTask() {
         // Code to be executed periodically
         Category[] categories = Category.values();
         for (Category category : categories) {
             newsService.parseApi(category);
-        }        
+        }
         long totalEntries = newsService.getTotalEntries();
 
-        // If the number of entries is greater than 500, reduce it to 500 by deleting oldest entries
-        if (totalEntries > 500) {
-            int entriesToDelete = (int) (totalEntries - 500);
+        // If the number of entries is greater than 500, reduce it to 500 by deleting
+        // oldest entries
+        if (totalEntries > 12000) {
+            int entriesToDelete = (int) (totalEntries - 12000);
             newsService.deleteOldestEntries(entriesToDelete);
         }
     }
